e3682b55274fa7acc79f8a70d38a6bf3c979e6ad
Sasikumar Muthusamy <66126244+SasikumarDev@users.noreply.github.com>
2021-08-10 08:53:45 +0530

    Update environment.prod.ts
    Update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   companyName: 'Nature Vitamins',
   companyMotto: 'feel the freshness',
   companyIconPath: 'assets/images/App_Icon.png',
-  supabaseURL: 'https://fvlmwzgvmutngnoloqrz.supabase.co',
-  supabaseKEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzcwMzk2MiwiZXhwIjoxOTQzMjc5OTYyfQ.vrF5YSzhP_9zB3Fse3FHFbAMWI__dkVPIR_mAdLZYnI'
+  supabaseURL: '<Your URL>',
+  supabaseKEY: '<Your Key>'
 };
